2ff95d642adbae699e7abcf69635414919ab55f4
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-01-04 15:13:25 +0100

    Add project files.

---
+++ OOP_Klasifikace/Program.cs
@@ -0,0 +1,21 @@
+﻿using OOP_Klasifikace;
+
+Student gebel = new Student("Jáchym Gebel");
+Student fagulec = new Student("Adam Fagulec");
+
+fagulec.PridatZnamkuProgramko(2);
+fagulec.PridatZnamkuAnglictina(1);
+fagulec.PridatZnamkuMatika(4);
+fagulec.PridatZnamkuCestina(5);
+fagulec.PridatZnamkuCestina(1);
+gebel.PridatZnamkuCestina(3);
+gebel.PridatZnamkuCestina(2);
+gebel.PridatZnamkuCestina(4);
+gebel.PridatZnamkuCestina(3);
+gebel.PridatZnamkuCestina(2);
+gebel.PridatZnamkuCestina(1);
+
+Console.WriteLine(gebel);
+Console.WriteLine(fagulec);
+
+Console.WriteLine(gebel.PrumerCestina());
---
+++ OOP_Klasifikace/Student.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Klasifikace
+{
+  public class Student
+  {
+    private string jmeno;
+    private List<int> cestina = new List<int>();
+    private List<int> anglictina = new List<int>();
+    private List<int> matika = new List<int>();
+    private List<int> programko = new List<int>();
+
+    public Student(string jmeno)
+    {
+      this.jmeno = jmeno;
+    }
+
+    public void PridatZnamkuCestina(int znamka)
+    {
+      cestina.Add(znamka);
+    }
+
+    public void PridatZnamkuAnglictina(int znamka)
+    {
+      anglictina.Add(znamka);
+    }
+
+    public void PridatZnamkuMatika(int znamka)
+    {
+      matika.Add(znamka);
+    }
+
+    public void PridatZnamkuProgramko(int znamka)
+    {
+      programko.Add(znamka);
+    }
+
+    public double PrumerCestina()
+    {
+      return cestina.Average();
+    }
+
+    public double PrumerAnglictina()
+    {
+      return anglictina.Average();
+    }
+
+    public double PrumerMatika()
+    {
+      return matika.Average();
+    }
+
+    public double PrumerProgramko()
+    {
+      return programko.Average();
+    }
+
+    public override string ToString()
+    {
+      string text = jmeno + "\r\n";
+      if(cestina.Count > 0)
+      {
+        text += "ČJ:  " + string.Join("; ", cestina) + " - " + PrumerCestina() + "\r\n";
+      }
+      else
+      {
+        text += "ČJ: \r\n";
+      }
+      if (matika.Count > 0)
+      {
+        text += "M:   " + string.Join("; ", matika) + " - " + PrumerMatika() + "\r\n";
+      }
+      else
+      {
+        text += "M: \r\n";
+      }
+      if (anglictina.Count > 0)
+      {
+        text += "AJ:  " + string.Join("; ", anglictina) + " - " + PrumerAnglictina() + "\r\n";
+      }
+      else
+      {
+        text += "AJ: \r\n";
+      }
+      if (programko.Count > 0)
+      {
+        text += "PRG: " + string.Join("; ", programko) + " - " + PrumerProgramko() + "\r\n";
+      }
+      else
+      {
+        text += "PRG: \r\n";
+      }
+      return text;
+    }
+  }
+}
